ca1b9f976c8ecae3b3543e3d6d4ddd591d3fd176
GoodDeeds <raosukrut@gmail.com>
2016-05-20 13:44:55 +0530

    Attempted problem 'MRECAMAN' - update 2

--- SPOJ/mrecaman(not yet solved)/mrecaman.cpp
+++ SPOJ/mrecaman(not yet solved)/mrecaman.cpp
@@ -3,7 +3,7 @@
 #include<vector>
 using namespace std;
 int *arr=(int*)malloc(sizeof(int)*500001);
-int a(int m)
+/*int a(int m)
 {
 	if(m==0)
 	{
@@ -20,14 +20,14 @@ int a(int m)
 		if(temp>m)
 		{
 			int flag=0;
-			//for(int j=0;j<m;j++)
-			//{
-				//if((temp-m)==a(j))
-				//{
-					//flag=1;
-					//break;
-				//}
-			//}
+			for(int j=0;j<m;j++)
+			{
+				if((temp-m)==a(j))
+				{
+					flag=1;
+					break;
+				}
+			}
 			if(flag==0)
 			{
 				arr[m]=temp-m;
@@ -45,6 +45,43 @@ int a(int m)
 			return arr[m];
 		}
 	}
+}*/
+int a(int m)
+{
+	if(m==0)
+	{
+		arr[m]=0;
+		return arr[m];
+	}
+	if(arr[m]!=-1)
+	{
+		return arr[m];
+	}
+	if((arr[m-1]-m)>0)
+	{
+		int flag=0;
+		for(int j=m-1;j>=0;j--)
+		{
+			if((arr[m-1]-m)==arr[j])
+			{
+				flag=1;
+				break;
+			}
+		}
+		if(flag==0)
+		{
+			arr[m]=arr[m-1]-m;
+		}
+		else
+		{
+			arr[m]=arr[m-1]+m;
+		}
+	}
+	else
+	{
+		arr[m]=arr[m-1]+m;
+	}
+	return arr[m];
 }
 int main()
 {
@@ -58,7 +95,18 @@ int main()
 	cin>>k;
 	while(k!=-1)
 	{
-		int j=a(k);
+		int j;
+		int x=k-1;
+		while(arr[x]==-1)
+		{
+			x--;
+			
+		}
+		for(int z=x+1;z<k;z++)
+		{
+			a(z);
+		}
+		j=a(k);
 		cout<<j<<endl;
 		cin>>k;
 	}
