9b9ea44998594eb8c2a9ceadf57a359a49dc90e0
GoodDeeds <raosukrut@gmail.com>
2016-05-20 12:59:48 +0530

    Attempted problem 'MRECAMAN'

---
+++ SPOJ/diehard(not yet solved)/diehard.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void change
+int main()
+{
+	long long int t,h,a,i,j,k,l;
+	cin>>t;
+	for(i=0;i<t;i++)
+	{
+		cin>>h>>a;
+		vector<vector<long long int> > arr(h,vector<long long int>(a,-1));
+		
+	}
+}
---
+++ SPOJ/mrecaman(not yet solved)/mrecaman.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+static int arr[500001];
+int a(int m)
+{
+	if(m==0)
+	{
+		return 0;
+	}
+	if(arr[m]!=-1)
+	{
+		return arr[m];
+	}
+	else
+	{
+		if(a(m-1)>m)
+		{
+			int flag=0;
+			for(int j=0;j<m;j++)
+			{
+				if((a(m-1)-m)==a(j))
+				{
+					flag=1;
+					break;
+				}
+			}
+			if(flag==0)
+			{
+				arr[m]=a(m-1)-m;
+				return arr[m];
+			}	
+			else
+			{
+				arr[m]=a(m-1)+m;
+				return arr[m];
+			}
+		}
+		else
+		{
+			arr[m]=a(m-1)+m;
+			return arr[m];
+		}
+	}
+}
+int main()
+{
+	for(int i=0;i<500001;i++)
+	{
+		arr[i]=-1;
+	}
+	arr[0]=0;
+	int k;
+	int l=a(400000);
+	cin>>k;
+	while(k!=-1)
+	{
+		int j=a(k);
+		cout<<j<<endl;
+		cin>>k;
+	}
+	
+	return 0;
+	
+}
+
